074ff948bbf3b9ab5469e1544b059cfdafbce788
vedanthelwatkar <vedanthelwatkar@gmail.com>
2024-11-09 01:17:13 +0530

    backend api changed

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  BASE_URL: 'https://hackernews-assignment.vercel.app/api/stories',
+  BASE_URL: 'https://hackernews-backend.vercel.app/api/stories',
 };
